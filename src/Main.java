6fe84c23123ae806d19c9b35117a6c5c7bc68c90
Bytes_coder <film_liu@126.com>
2018-04-19 11:55:17 +0800

    test-lad

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World2!");
         System.out.println("Hello you lass!");
+        System.out.println("Hello you lad!");
 
     }
 }
